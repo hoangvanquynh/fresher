77703427152ef0bc56fc95301e980eee4bc0c082
vu phan viet thang <vuphan.viet@ascendcorp.com>
2022-05-24 22:32:37 +0700

    second commit

--- src/fa/training/main/Test.java
+++ src/fa/training/main/Test.java
@@ -3,15 +3,15 @@ package fa.training.main;
 import fa.training.entities.Customer;
 import fa.training.services.CustomerService;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         CustomerService customerService = new CustomerService();
-        List<Customer> customerList = new ArrayList<>();
 
         int choose;
         do {
@@ -20,24 +20,37 @@ public class Test {
             System.out.println("2. Show all Customers");
             System.out.println("3. Search Customer by phone number");
             System.out.println("4. Remove Customer by phone number");
-            System.out.println("5. Exit");
+            System.out.println("5. Save customer to file");
+            System.out.println("6. Exit");
             System.out.print("Your choice: ");
             choose = Integer.parseInt(scanner.nextLine());
+            String phone;
             switch (choose) {
                 case 1:
-                    customerList = customerService.createCustomer();
+                    customerService.createCustomer();
                     System.out.println("Successful");
                     break;
                 case 2:
-                    customerService.findAll(customerList);
+                    customerService.findAll();
                     break;
                 case 3:
-                    customerService.findByPhoneNumber(customerList);
+                    System.out.print("Enter phone number: ");
+                    phone = scanner.nextLine();
+                    customerService.findByPhoneNumber(phone);
+                    break;
+                case 4:
+                    System.out.print("Enter phone number: ");
+                    phone = scanner.nextLine();
+                    customerService.remove(phone);
+                    break;
+                case 5:
+                    customerService.save();
+                    System.out.println("Successful");
                     break;
                 default:
-                    System.out.println("choose 1 - 5");
+                    System.out.println("choose 1 - 6");
                     break;
             }
-        } while (choose != 5);
+        } while (choose != 6);
     }
 }
--- src/fa/training/services/CustomerService.java
+++ src/fa/training/services/CustomerService.java
@@ -4,17 +4,23 @@ import fa.training.entities.Customer;
 import fa.training.entities.Order;
 import fa.training.utils.Validator;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class CustomerService {
     Scanner scanner = new Scanner(System.in);
+    List<Customer> customerList = new ArrayList<>();
+
     public List<Customer> createCustomer() {
         boolean status;
         String flag;
-        List<Customer> customerList = new ArrayList<>();
         Customer customer = new Customer();
         System.out.print("Enter name: ");
         customer.setName(scanner.nextLine());
@@ -28,20 +34,20 @@ public class CustomerService {
             }
         } while (status);
         System.out.print("Enter address: ");
-        customer.setName(scanner.nextLine());
+        customer.setAddress(scanner.nextLine());
 
         // create orders
         List<Order> orderList = new ArrayList<>();
         int i = 1;
         do {
-            System.out.println("Enter order infor " + i +": ");
+            System.out.println("Enter order infor " + i + ": ");
             Order order = new Order();
             do {
                 System.out.print("  + number: ");
                 order.setNumber(scanner.nextLine());
                 if (order.getNumber().length() <= 10) {
                     status = false;
-                }else {
+                } else {
                     status = true;
                 }
             } while (status);
@@ -49,13 +55,12 @@ public class CustomerService {
             do {
                 System.out.print("  + date: ");
                 try {
-                    order.setDate(simpleDateFormat.parse(scanner.next()));
+                    order.setDate(simpleDateFormat.parse(scanner.nextLine()));
                     status = false;
                 } catch (Exception e) {
                     status = true;
                 }
             } while (status);
-            scanner.nextLine();
             System.out.print("Do you want add order? stop(enter 'n' or 'N'): ");
             flag = scanner.nextLine();
             orderList.add(order);
@@ -66,7 +71,7 @@ public class CustomerService {
         return customerList;
     }
 
-    public void findAll(List<Customer> customerList) {
+    public void findAll() {
         if (customerList.size() > 0) {
             for (Customer customer : customerList) {
                 System.out.println(customer.toString());
@@ -76,15 +81,29 @@ public class CustomerService {
         }
     }
 
-    public void findByPhoneNumber(List<Customer> customerList) {
-        System.out.print("Enter phone number: ");
-        String phoneNumber = scanner.nextLine();
+    public void findByPhoneNumber(String phone) {
         for (Customer customer : customerList) {
-            if (customer.getPhoneNumber().equals(phoneNumber)) {
+            if (customer.getPhoneNumber().equals(phone)) {
                 System.out.println(customer);
-            }else{
+            } else {
                 System.out.println("Customer not found");
             }
         }
+    }
+
+    public void remove(String phone) {
+        for (Customer customer : customerList) {
+            if (customer.getPhoneNumber().equals(phone)) {
+                customerList.remove(customer);
+                System.out.println("Delete successful");
+            } else {
+                System.out.println("Customer not found");
+            }
+        }
+    }
+    public void save() throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("customer.dat"));
+        bufferedWriter.append(customerList.toString());
+        bufferedWriter.close();
     }
 }
